2383ee86367124af4388f94ba8a043b4f26b1480
test <medya@test.com>
2025-08-04 09:18:10 +0300

    template
    start

---
+++ index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link href="./style.css" rel="stylesheet">
+  <link rel="icon" type="image/x-icon" href="./files/fav/note.png" sizes="32x32">
+
+  <!-- icons -->
+  <link rel="stylesheet" href="./files/images/icons/fonts/basic/boxicons.min.css">
+  <link rel="stylesheet" href="./files/images/icons/fonts/brands/boxicons-brands.min.css">
+  <link rel="stylesheet" href="./files/images/icons/fonts/animations.min.css">
+  <link rel="stylesheet" href="./files/images/icons/fonts/transformations.min.css">
+  <!-- icons -->
+
+  <title>HTML Components</title>
+
+</head>
+<body>
+
+  <div data-component="parent" class="mx-auto max-w-7xl lg:px-2 lg:py-2">
+
+    <!-- COMPONENTS -->
+
+    <?php include 'components/header.html'; ?>
+    <?php include 'components/cards.html'; ?>
+
+    <!-- COMPONENTS -->
+
+  </div>
+
+</body>
+</html>
+
+<script>
+
+  // dynamic
+
+</script>
