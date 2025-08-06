b79c584ad8d4edd46830abaffab2fce5e5d02bc3
test <medya@test.com>
2025-08-06 14:41:21 +0300

    about
    page

---
+++ about.php
@@ -0,0 +1,51 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link href="./style.css" rel="stylesheet">
+  <link rel="icon" type="image/x-icon" href="./files/fav/note.png" sizes="32x32">
+
+  <!-- icons -->
+  <link rel="stylesheet" href="./files/images/icons/fonts/basic/boxicons.min.css">
+  <link rel="stylesheet" href="./files/images/icons/fonts/brands/boxicons-brands.min.css">
+  <link rel="stylesheet" href="./files/images/icons/fonts/animations.min.css">
+  <link rel="stylesheet" href="./files/images/icons/fonts/transformations.min.css">
+  <!-- icons -->
+
+  <title>HTML Components</title>
+
+</head>
+<body>
+
+  <div data-component="parent" class="relative w-full bg-neutral-100">
+
+    <!-- COMPONENTS -->
+
+    <?php include 'components/header-2.html'; ?>
+    <?php include 'components/entrance.html'; ?>
+
+    <?php include 'components/section.html'; ?>
+    <?php include 'components/section-information.html'; ?>
+    <?php include 'components/section-cards.html'; ?>
+
+    <?php include 'components/company-information.html'; ?>
+
+    <?php include 'components/footer-state.html'; ?>
+    <?php include 'components/footer.html'; ?>
+
+    <!-- COMPONENTS -->
+
+    <?php include 'components/go-to-top.html'; ?>
+
+  </div>
+
+</body>
+</html>
+
+<script>
+
+  // dynamic
+
+</script>
