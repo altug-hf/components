585b5a123fdb78bc0bc762dec3fde0bbfa8a0b56
test <medya@test.com>
2025-08-29 14:13:39 +0300

    kurumsal 2
    2

---
+++ values.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+  <link href="./files/style.css" rel="stylesheet">
+  <link rel="icon" type="image/x-icon" href="./files/fav/note.png" sizes="32x32">
+
+  <!-- icons -->
+  <link rel="stylesheet" href="./files/images/icons/fonts/basic/boxicons.min.css">
+  <link rel="stylesheet" href="./files/images/icons/fonts/brands/boxicons-brands.min.css">
+  <link rel="stylesheet" href="./files/images/icons/fonts/animations.min.css">
+  <link rel="stylesheet" href="./files/images/icons/fonts/transformations.min.css">
+  <!-- icons -->
+
+  <! -- Splide Slider 4.13 -->
+  <link rel="stylesheet" href="../files/slider/css/splide.min.css">
+  <link rel="stylesheet" href="../files/slider/css/themes/splide-default.min.css">
+  <! -- Splide Slider 4.13 -->
+
+  <title>HTML Components</title>
+
+</head>
+<body>
+
+  <div data-component="parent" class="relative w-full bg-neutral-100">
+
+    <!-- COMPONENTS -->
+
+    <?php include 'webratik/header-2.html'; ?>
+
+    <?php include 'webratik/values/section-1.html'; ?>
+    <?php include 'webratik/values/section.html'; ?>
+
+    <?php include 'webratik/footer-state.html'; ?>
+    <?php include 'webratik/footer.html'; ?>
+
+    <!-- COMPONENTS -->
+
+    <?php include 'components/go-to-top.html'; ?>
+
+  </div>
+
+</body>
+</html>
+
+<script>
+
+  // dynamic
+
+</script>
+
+<!-- Splide JavaScript -->
+<script src="../files/slider/js/splide.min.js"></script>
